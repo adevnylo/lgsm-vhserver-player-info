8d6f3f83a061d3dff476870f033a1c5d4fe79ade
adevnylo <adnylo+dev@mailbox.org>
2023-07-24 14:15:58 +0200

    Fix and refactor vhserver_player_info.py
    After noticing that running the previous version through the systemd service caused the script to continuously print the player database inside the "output log file", I decided to change the execution logic. Now, the default behaviour is to list the player database, while launching the script with the "--daemon" parameter executes the background process.
    
    ChatGPT helped with the code refactoring and formatting, so now the code looks cleaner (and it has classes now).
    I've also removed the "player_serial" and replaced it with the "player_steamID", and added a "player_game_sessions" variable to show how often the player connects to the server (I still have to understand how to consistently ascertain when a specific player disconnects from the server).

--- vhserver_player_info.py
+++ vhserver_player_info.py
@@ -3,201 +3,256 @@ import sys
 import json
 import time
 import subprocess
-import requests
 import signal
+import logging
+import traceback
 from datetime import datetime
 
-# User-configurable variables
 STEAM_API_KEY = "YOUR_STEAM_API_KEY"
-LOG_FILE_PATH = "/home/vhserver/log/console/vhserver-console.log" # Edit only if you changed the default log file from the LGSM launch parameters.
+LOG_FILE_PATH = "/home/vhserver/log/console/vhserver-console.log"
+DAEMON_INTERVAL = 300
 
-# Function to create and activate the virtual environment
-def create_virtualenv():
-    try:
-        subprocess.check_call(["python3", "-m", "venv", "venv"])
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to create virtual environment: {e}")
-        sys.exit(1)
+# Set up logging
+logging.basicConfig(filename="vhserver_player_info.log", level=logging.INFO)
+logger = logging.getLogger("vhserver_player_info")
 
-# Function to activate the virtual environment
-def activate_virtualenv():
-    activate_command = os.path.join("venv", "bin", "activate")
-    if os.path.exists(activate_command):
-        subprocess.Popen(f"source {activate_command}", shell=True, executable="/bin/bash")
 
-# Function to read the log file and extract player info
-def process_log_file(log_file, api_key):
-    # Read the last check time from last_check_time.json
-    last_check_time = read_last_check_time()
+class VirtualEnvironmentManager:
+    @staticmethod
+    def create_virtualenv():
+        try:
+            subprocess.check_call(["python3", "-m", "venv", "venv"])
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Failed to create virtual environment: {e}")
+            sys.exit(1)
 
-    # If last_check_time is None, set it to the current time
-    if last_check_time is None:
-        last_check_time = datetime.now()
+    @staticmethod
+    def activate_virtualenv():
+        activate_command = os.path.join("venv", "bin", "activate")
+        if os.path.exists(activate_command):
+            subprocess.Popen(f"source {activate_command}", shell=True, executable="/bin/bash")
 
-    # Check if player_info.json exists and load its content
-    if os.path.exists("player_info.json") and os.path.getsize("player_info.json") > 0:
-        with open("player_info.json", "r") as f:
-            player_info_data = json.load(f)
-    else:
-        player_info_data = []
+    @staticmethod
+    def install_required_packages():
+        try:
+            subprocess.check_call(["venv/bin/pip", "install", "requests"])
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Failed to install required packages: {e}")
+            sys.exit(1)
 
-    # Read the log file and process player info
-    with open(log_file, "r") as f:
-        lines = f.readlines()
 
-        # Initialize variables to store player info
-        player_steamID = None
-        player_name = None
-        player_steam_name = None
-        time_last_seen = None
+class PlayerInfoProcessor:
+    def __init__(self):
+        self.player_data = self.load_player_data()
+        self.last_check_time = self.read_last_check_time()
 
-        # Iterate through each line in the log file
-        for line in lines:
-            # Check if the line contains "received local Platform ID Steam_"
-            if "received local Platform ID Steam_" in line:
-                player_steamID = line.split("received local Platform ID Steam_")[1][:17]
-                time_last_seen = datetime.strptime(line[:19], "%m/%d/%Y %H:%M:%S")  # Set time_last_seen to the first 19 characters of the log line
+    def load_player_data(self):
+        try:
+            with open("player_database.json", "r") as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
 
-            # Check if the line contains "Got character ZDOID from"
-            elif "Got character ZDOID from" in line:
-                player_name = line.split("Got character ZDOID from ")[1].split(" :")[0]
+    def read_last_check_time(self):
+        try:
+            with open("last_check_time.json", "r") as f:
+                data = json.load(f)
+                return datetime.strptime(data.get("last_check_time"), "%m/%d/%Y %H:%M:%S")
+        except (FileNotFoundError, json.JSONDecodeError):
+            return None
 
-                # Use the Steam API to get the Steam nickname
-                player_steam_name = get_steam_nickname(player_steamID, api_key)
+    def get_steam_nickname(self, steamID):
+        try:
+            import requests
 
-                # Check if the player_steamID is already processed with the same time_last_seen
-                if not is_steamID_processed(player_steamID, time_last_seen, player_info_data):
-                    # Create a dictionary with player info and append it to player_info_data
-                    player_info = {
-                        "player_serial": len(player_info_data) + 1,
-                        "player_name": player_name,
-                        "player_steamID": player_steamID,
-                        "player_steam_name": player_steam_name,
-                        "time_last_seen": time_last_seen
-                    }
-                    player_info_data.append(player_info)
+            url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={steamID}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
 
-                    # Print the player info
-                    print_player_info(player_info)
+            players = data.get("response", {}).get("players", [])
+            if players:
+                return players[0].get("personaname", "Unknown")
+            else:
+                raise ValueError("Steam API response does not contain player information.")
+        except requests.RequestException as e:
+            raise RuntimeError(f"Failed to fetch Steam nickname for SteamID {steamID}: {e}")
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+            raise RuntimeError(f"Invalid response from Steam API for SteamID {steamID}: {e}")
 
-                # If the player_steamID is already processed and the player_name is different, add it as a new entry
-                elif player_steam_name != get_steam_name_for_steamID(player_steamID, player_info_data):
-                    player_info_data.append({
-                        "player_serial": len(player_info_data) + 1,
-                        "player_name": f"{player_name}_{len(player_info_data) + 1}",
-                        "player_steamID": player_steamID,
-                        "player_steam_name": player_steam_name,
-                        "time_last_seen": time_last_seen
-                    })
-                    print(f"Player {player_steamID} already exists with a different name. Adding new entry.")
+    def is_steamID_processed(self, steamID, time_last_seen):
+        return any(
+            player_info["player_steamID"] == steamID and player_info.get("time_last_seen") == time_last_seen
+            for player_info in self.player_data
+        )
 
-            # Check if it's time to stop processing the log file
-            log_time = get_log_time(line)
-            if log_time and log_time > last_check_time:
-                break
+    def get_log_time(self, line):
+        try:
+            time_str = line.split("[", 1)[1].split("]", 1)[0]
+            return datetime.strptime(time_str, "%m/%d/%Y %H:%M:%S")
+        except (ValueError, IndexError):
+            return None
 
-    # Write the updated player_info_data to player_info.json
-    with open("player_info.json", "w") as f:
-        json.dump(player_info_data, f, indent=4)
+    def update_player_data(self, player_steamID, player_steam_name, player_name, player_game_sessions, time_last_seen):
+        try:
+            time_last_seen = datetime.strptime(time_last_seen, "%m/%d/%Y %H:%M:%S")
+        except ValueError:
+            raise ValueError(f"Invalid date and time format for time_last_seen: {time_last_seen}")
 
-    # Write the current time to last_check_time.json
-    current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    with open("last_check_time.json", "w") as f:
-        json.dump({"last_check_time": current_time}, f)
-
-# Function to read the last check time from last_check_time.json
-def read_last_check_time():
-    if os.path.exists("last_check_time.json") and os.path.getsize("last_check_time.json") > 0:
-        with open("last_check_time.json", "r") as f:
-            data = json.load(f)
-            return datetime.strptime(data.get("last_check_time"), "%m/%d/%Y %H:%M:%S")
-    return None
-
-# Function to get the Steam nickname using the Steam API
-def get_steam_nickname(steamID, api_key):
-    try:
-        url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steamID}"
-        response = requests.get(url)
-        data = response.json()
-        return data["response"]["players"][0]["personaname"]
-    except requests.RequestException as e:
-        print(f"Failed to fetch Steam nickname for SteamID {steamID}: {e}")
-        return "Unknown"
-
-# Function to check if a player_steamID is already processed with the same time_last_seen
-def is_steamID_processed(steamID, time_last_seen, player_info_data):
-    for player_info in player_info_data:
-        if player_info["player_steamID"] == steamID and player_info.get("time_last_seen") == time_last_seen:
+        existing_player_info = next((p for p in self.player_data if p.get("player_steamID") == player_steamID), None)
+        if existing_player_info:
+            existing_time_last_seen = datetime.strptime(existing_player_info.get("time_last_seen", "01/01/1900 00:00:00"), "%m/%d/%Y %H:%M:%S")
+            if time_last_seen > existing_time_last_seen:
+                existing_player_info["player_steam_name"] = list(
+                    set(existing_player_info.get("player_steam_name", []) + [player_steam_name])
+                )
+                existing_player_info["player_name"] = list(set(existing_player_info.get("player_name", []) + [player_name]))
+                existing_player_info["player_game_sessions"] = existing_player_info.get("player_game_sessions", 0) + 1
+                existing_player_info["time_last_seen"] = time_last_seen.strftime("%m/%d/%Y %H:%M:%S")
             return True
-    return False
+        return False
 
-# Function to get the Steam name for a specific player_steamID
-def get_steam_name_for_steamID(steamID, player_info_data):
-    for player_info in player_info_data:
-        if player_info["player_steamID"] == steamID:
-            return player_info.get("player_steam_name")
-    return None
+    def process_log_file(self, log_file):
+        if self.last_check_time is None:
+            self.last_check_time = datetime.now()
 
-# Function to get the time from a log line
-def get_log_time(line):
-    try:
-        time_str = line.split("[", 1)[1].split("]", 1)[0]
-        return datetime.strptime(time_str, "%m/%d/%Y %H:%M:%S")
-    except (ValueError, IndexError):
-        return None
+        try:
+            import requests
 
-# Function to print player info
-def print_player_info(player_info):
-    print(f"Player Serial: {player_info.get('player_serial', 'N/A')}")
-    print(f"Player Name: {player_info.get('player_name', 'N/A')}")
-    print(f"Player SteamID: {player_info.get('player_steamID', 'N/A')}")
-    print(f"Player Steam Name: {player_info.get('player_steam_name', 'N/A')}")
-    print(f"Time Last Seen: {player_info.get('time_last_seen', 'N/A')}")
-    print()
+            with open(log_file, "r") as f:
+                lines = f.readlines()
 
-# Function to handle SIGTERM and SIGINT signals
-def handle_signals(signum, frame):
-    # Write the current time to last_check_time.json
-    current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    with open("last_check_time.json", "w") as f:
-        json.dump({"last_check_time": current_time}, f)
+                player_steamID = None
+                player_steam_name = None
+                player_name = None
+                player_game_sessions = 0
+                time_last_seen = "01/01/1900 00:00:00"
 
-    # Exit the script
-    sys.exit(0)
+                players = {}  # Using player_steamID as the key
+
+                for line in lines:
+                    if "received local Platform ID Steam_" in line:
+                        player_steamID = line.split("received local Platform ID Steam_")[1][:17]
+                        time_last_seen = line[:19]
+
+                    elif "Got character ZDOID from" in line:
+                        player_name = line.split("Got character ZDOID from ")[1].split(" :")[0]
+                        player_steam_name = self.get_steam_nickname(player_steamID)
+
+                        if not self.is_steamID_processed(player_steamID, time_last_seen):
+                            players[player_steamID] = {
+                                "player_steam_name": [player_steam_name],
+                                "player_name": [player_name],
+                                "player_game_sessions": 1,
+                                "time_last_seen": time_last_seen,
+                            }
+                        elif player_steam_name != self.get_steam_nickname(player_steamID):
+                            if player_steamID not in players:
+                                players[player_steamID] = {
+                                    "player_steam_name": [player_steam_name],
+                                    "player_name": [player_name],
+                                    "player_game_sessions": 1,
+                                    "time_last_seen": time_last_seen,
+                                }
+                            else:
+                                players[player_steamID]["player_steam_name"] = list(
+                                    set(players[player_steamID]["player_steam_name"] + [player_steam_name])
+                                )
+
+                    log_time = self.get_log_time(line)
+                    if log_time and log_time > self.last_check_time:
+                        break
+
+            # Initialize player_data if "player_database.json" is empty
+            if not self.player_data:
+                self.player_data = []
+
+            # Update player_data with aggregated data
+            for player_steamID, player_info in players.items():
+                self.update_player_data(player_steamID, player_info["player_steam_name"][0], player_info["player_name"][0], player_info["player_game_sessions"], player_info["time_last_seen"])
+
+            # Add new players to player_data
+            for player_steamID, player_info in players.items():
+                if player_steamID not in {player.get("player_steamID") for player in self.player_data}:
+                    self.player_data.append(
+                        {
+                            "player_steamID": player_steamID,
+                            "player_steam_name": player_info["player_steam_name"],
+                            "player_name": player_info["player_name"],
+                            "player_game_sessions": 1,
+                            "time_last_seen": player_info["time_last_seen"],
+                        }
+                    )
+
+            # Write the updated player_data to player_database.json
+            with open("player_database.json", "w") as f:
+                json.dump(self.player_data, f, indent=4)
+
+            current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            with open("last_check_time.json", "w") as f:
+                json.dump({"last_check_time": current_time}, f)
+
+        except Exception as e:
+            logger.error(f"An error occurred while processing log file:\n{traceback.format_exc()}")
+
+
+    def print_player_info(self, player_info):
+        time_last_seen = player_info.get("time_last_seen", "N/A")
+        try:
+            formatted_time_last_seen = datetime.strptime(time_last_seen, "%m/%d/%Y %H:%M:%S").strftime("%Y/%m/%d %H:%M:%S")
+        except ValueError:
+            formatted_time_last_seen = "Invalid Date and Time Format"
+
+        print(f"Player SteamID: {player_info.get('player_steamID', 'N/A')}")
+        print(f"Player Steam Name: {', '.join(player_info.get('player_steam_name', ['N/A']))}")
+        print(f"Player Name: {', '.join(player_info.get('player_name', ['N/A']))}")
+        print(f"Game Sessions: {player_info.get('player_game_sessions', 0)}")
+        print(f"Time Last Seen: {formatted_time_last_seen}")
+        print()
+
+    def print_player_database(self):
+        if self.player_data:
+            for player_info in self.player_data:
+                self.print_player_info(player_info)
+        else:
+            print("No player information found.")
+
+    def handle_signals(self, signum, frame):
+        current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        with open("last_check_time.json", "w") as f:
+            json.dump({"last_check_time": current_time}, f)
+
+        sys.exit(0)
+
 
 def main():
     # Check if the virtual environment exists and create it if it doesn't
+    venv_manager = VirtualEnvironmentManager()
     if not os.path.exists("venv"):
-        create_virtualenv()
+        venv_manager.create_virtualenv()
+        venv_manager.activate_virtualenv()
+        venv_manager.install_required_packages()
 
-    # Activate the virtual environment
-    activate_virtualenv()
+    # Initialize the player info processor
+    player_info_processor = PlayerInfoProcessor()
 
     # Register the signal handler for SIGTERM and SIGINT
-    signal.signal(signal.SIGTERM, handle_signals)
-    signal.signal(signal.SIGINT, handle_signals)
+    signal.signal(signal.SIGTERM, player_info_processor.handle_signals)
+    signal.signal(signal.SIGINT, player_info_processor.handle_signals)
 
-    # Check if the script was run by systemd or with the "--list" parameter
-    if not os.path.basename(sys.argv[0]) == "systemd" and len(sys.argv) == 1:
-        # If not run by systemd and no other arguments provided, execute "--list" by default
-        sys.argv.append("--list")
-
-    # Check if the script was run with the "--list" parameter
-    if len(sys.argv) > 1 and sys.argv[1] == "--list":
-        # Check if player_info.json exists and load its content
-        if os.path.exists("player_info.json") and os.path.getsize("player_info.json") > 0:
-            with open("player_info.json", "r") as f:
-                player_info_data = json.load(f)
-                for player_info in player_info_data:
-                    print_player_info(player_info)
-        else:
-            print("No player information found.")
+    # Check if the script was run with the "--daemon" parameter
+    if len(sys.argv) > 1 and sys.argv[1] == "--daemon":
+        try:
+            while True:
+                if os.path.exists(LOG_FILE_PATH):
+                    player_info_processor.process_log_file(LOG_FILE_PATH)
+                time.sleep(DAEMON_INTERVAL)
+        except Exception as e:
+            logger.error(f"An unexpected error occurred in the daemon:\n{traceback.format_exc()}")
+            sys.exit(1)
     else:
-        # Check and process the log file every 10 minutes
-        while True:
-            if os.path.exists(LOG_FILE_PATH):
-                process_log_file(LOG_FILE_PATH, STEAM_API_KEY)
-            time.sleep(600)
+        player_info_processor.print_player_database()
+
 
 if __name__ == "__main__":
     main()
